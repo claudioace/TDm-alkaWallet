57b6ce78bd824312c3db31c393f5d3267f0eb3ac
claudioace <claudioace@gmail.com>
2026-01-09 19:08:04 -0300

    landing basic okay

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+  $(document).click(function(){
+  window.location.href = 'login.html'
+  })  
+
+})
---
+++ assets/login.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $('.btn').click(function(){
+  const email = $('#inputEmail').val; 
+  console.log(email);
+
+  const pass = $('#inputPassword').val;  
+
+  })  
+
+})
