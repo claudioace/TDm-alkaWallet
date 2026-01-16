141f3e8b6b24d8d9ffbd828c3471f2fe5eb5f4b3
claudioace <claudioace@gmail.com>
2026-01-16 15:20:46 -0300

    16-01-2026 retomando proyecto

--- assets/login.js
+++ assets/login.js
@@ -1,11 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $('.btn').click(function(){
-  const email = $('#inputEmail').val; 
-  console.log(email);
-
-  const pass = $('#inputPassword').val;  
-
-  })  
+   /* $('.btn').click(function () {
+        const email = $('#inputEmail').val();
+        const pass = $('#inputPassword').val();
+        console.log(pass);
+        console.log(email);
+        //validar contra  ¿??
+        const usuarioValido = true;
+        if (usuarioValido){
+            window.location.href = 'menu.html'
+        };
+  });
+*/
+    $('#formLogin').on('submit', function(e){
+        e.preventDefault();
+        const email = $('#inputEmail').val();
+        const pass = $('#inputPassword').val();
+        console.log(pass);
+        console.log(email);
+        //validar contra  ¿??
+        const usuarioValido = true;
+        if (usuarioValido){
+            window.location.href = 'menu.html'
+        };
+    })
 
 })
