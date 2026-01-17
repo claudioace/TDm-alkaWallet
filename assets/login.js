cd289a282be7595e9d1ad837abb6099508fc2b2b
claudioace <claudioace@gmail.com>
2026-01-17 18:59:38 -0300

    valiadciones login y registro

--- assets/login.js
+++ assets/login.js
@@ -10,26 +10,53 @@ const modal = $('#modalRegistro');
         e.preventDefault();
         const email = $('#inputEmail').val();
         const pass = passLog.val();
-        console.log(pass);
-        console.log(email);
-//!!!!validar contra  ¿??
-        const usuarioValido = true;
-        if (usuarioValido){
-            window.location.href = 'menu.html'
+        const usuarioValido = false;
+
+        const users = getUsers();
+        const storedUser = users.find((user) => user.email === email);
+
+        if (storedUser && storedUser.password === pass){
+            this.reset;
+            window.location.href = 'menu.html';
         };
     });
 
+    function getUsers() {
+    const response = JSON.parse(localStorage.getItem("users"));
+    const users = [];
+
+    if (response) {
+      users.push(...response);
+    }
+    return users;
+  }    
+
+
+    //llamr a funcion que almacena en localstorage
     formRegistro.on('submit', function(e){
         e.preventDefault();
         const name = $('#nombreReg').val();
         const lastName = $('#apellidoReg').val();
         const email = $('#emailReg').val();
         const pass = $('#passwordReg').val();
-        console.log(name, lastName, email, pass);
-        
-//!!!!escribir en memoria
 
+        //genera un objeto userdata
+        const userData = {
+            name: name,
+            lastName: lastName,
+            email: email,
+            password: pass,
+        };
+        
+        //llama a la funcion get user para guardar en localstorage
+        const users = getUsers();
+        users.push(userData);
+        localStorage.setItem("users", JSON.stringify(users));
+        this.reset();        
+        console.log(users);
+        
     });
+
     //memo: estos permiten ver u ocultar el password 
     mostrarPass.on('change', function(){
         if ($(this).is(':checked')) {
